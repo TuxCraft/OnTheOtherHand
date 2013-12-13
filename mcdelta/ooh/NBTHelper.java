915a1e2aa6ccd46e6ae2d24e06b4b98ad411c919
TuxCraft <tux.craft.42@gmail.com>
2013-12-13 06:56:42 -0500

    12/13 6:56

--- mcdelta/ooh/NBTHelper.java
+++ mcdelta/ooh/NBTHelper.java
@@ -1,23 +1,47 @@
 package mcdelta.ooh;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 
 public class NBTHelper
 {
 	public static final String	OOH_COMPOUND_KEY	= "OOHData";
 	public static final String	DUAL_WIELD	     = "DualWield";
+	public static final String	OFFHAND_WEAPON	 = "OffhandWeapon";
 
 
 
 
-	public static NBTTagCompound getPlayerNBT (EntityPlayer player)
+	public static NBTTagCompound getOOHNBT (EntityPlayer player)
 	{
 		if (!player.getEntityData().hasKey(OOH_COMPOUND_KEY))
 		{
-			player.getEntityData().setCompoundTag(OOH_COMPOUND_KEY, new NBTTagCompound());
+			NBTTagCompound compound = new NBTTagCompound();
+			compound.setBoolean(DUAL_WIELD, true);
+			refresh(compound, player);
+			player.getEntityData().setCompoundTag(OOH_COMPOUND_KEY, compound);
 		}
 
 		return player.getEntityData().getCompoundTag(OOH_COMPOUND_KEY);
+	}
+
+
+
+
+	public static void refresh (EntityPlayer player)
+	{
+		refresh(getOOHNBT(player), player);
+	}
+
+
+
+
+	public static void refresh (NBTTagCompound compound, EntityPlayer player)
+	{
+		ItemStack stack = player.inventory.getStackInSlot(8);
+		compound.setCompoundTag(OFFHAND_WEAPON, stack == null ? new NBTTagCompound() : stack.writeToNBT(new NBTTagCompound()));
+		
+		Assets.p(stack);
 	}
 }
--- mcdelta/ooh/PlayerTracker.java
+++ mcdelta/ooh/PlayerTracker.java
@@ -6,27 +6,36 @@ import cpw.mods.fml.common.IPlayerTracker;
 public class PlayerTracker implements IPlayerTracker
 {
 	@Override
-    public void onPlayerLogin (EntityPlayer player)
-    {
-	    
-    }
+	public void onPlayerLogin (EntityPlayer player)
+	{
+		NBTHelper.refresh(player);
+	}
+
+
+
 
 	@Override
-    public void onPlayerLogout (EntityPlayer player)
-    {
-	    
-    }
+	public void onPlayerLogout (EntityPlayer player)
+	{
+
+	}
+
+
+
 
 	@Override
-    public void onPlayerChangedDimension (EntityPlayer player)
-    {
-	     
-    }
+	public void onPlayerChangedDimension (EntityPlayer player)
+	{
+
+	}
+
+
+
 
 	@Override
-    public void onPlayerRespawn (EntityPlayer player)
-    {
-	    
-    }
+	public void onPlayerRespawn (EntityPlayer player)
+	{
+
+	}
 
 }
