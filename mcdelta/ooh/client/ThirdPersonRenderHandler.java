0b1f89b15f375eebdf9c9600f66ab52bd776eda8
TuxCraft <tux.craft.42@gmail.com>
2013-12-13 21:44:58 -0500

    12/13 9:44

--- mcdelta/ooh/client/ThirdPersonRenderHandler.java
+++ mcdelta/ooh/client/ThirdPersonRenderHandler.java
@@ -46,6 +46,34 @@ public class ThirdPersonRenderHandler
 
 
 	@ForgeSubscribe
+	public void renderSecondHand (RenderPlayerEvent.Pre event)
+	{
+		try
+		{
+			EntityPlayer player = event.entityPlayer;
+			OOHData data = OOHData.getOOHData(player);
+			RenderPlayer renderer = event.renderer;
+
+			if (modelBipedMain == null)
+			{
+				Field fi1 = renderer.getClass().getDeclaredField("modelBipedMain");
+				fi1.setAccessible(true);
+				modelBipedMain = (ModelBiped) fi1.get(renderer);
+			}
+
+			ItemStack stack = data.doubleEngaged ? data.secondItem : null;
+			modelBipedMain.heldItemLeft = stack == null ? 0 : 1;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+
+
+
+	@ForgeSubscribe
 	public void renderSecondHand (RenderPlayerEvent.Specials.Pre event)
 	{
 		try
@@ -54,169 +82,169 @@ public class ThirdPersonRenderHandler
 			OOHData data = OOHData.getOOHData(player);
 			RenderPlayer renderer = event.renderer;
 
-			Field fi1 = renderer.getClass().getDeclaredField("modelBipedMain");
-			fi1.setAccessible(true);
-			modelBipedMain = (ModelBiped) fi1.get(renderer);
+			if (modelBipedMain == null)
+			{
+				Field fi1 = renderer.getClass().getDeclaredField("modelBipedMain");
+				fi1.setAccessible(true);
+				modelBipedMain = (ModelBiped) fi1.get(renderer);
+			}
 
-			Field fi2 = renderer.getClass().getSuperclass().getSuperclass().getDeclaredField("renderManager");
-			fi2.setAccessible(true);
-			renderManager = (RenderManager) fi2.get(renderer);
+			if (renderManager == null)
+			{
+				Field fi2 = renderer.getClass().getSuperclass().getSuperclass().getDeclaredField("renderManager");
+				fi2.setAccessible(true);
+				renderManager = (RenderManager) fi2.get(renderer);
+			}
 
 			if (data != null)
 			{
-					Class[] param1 = new Class[]
-					{ net.minecraft.entity.Entity.class };
-					getEntityTexture = renderer.getClass().getDeclaredMethod("getEntityTexture", param1);
-					getEntityTexture.setAccessible(true);
+				Class[] param1 = new Class[]
+				{ net.minecraft.entity.Entity.class };
+				getEntityTexture = renderer.getClass().getDeclaredMethod("getEntityTexture", param1);
+				getEntityTexture.setAccessible(true);
 
-					renderManager.renderEngine.bindTexture((ResourceLocation) getEntityTexture.invoke(event.renderer, new Object[]
-					{ player }));
+				renderManager.renderEngine.bindTexture((ResourceLocation) getEntityTexture.invoke(event.renderer, new Object[]
+				{ player }));
 
-					arm = new ModelRenderer(modelBipedMain, 40, 16);
-					arm.mirror = true;
-					arm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
-					arm.setRotationPoint(5.0F, 2.0F + 0.0F, 0.0F);
+				arm = new ModelRenderer(modelBipedMain, 40, 16);
+				arm.mirror = true;
+				arm.addBox(-1.0F, -2.0F, -2.0F, 4, 12, 4, 0.0F);
+				arm.setRotationPoint(5.0F, 2.0F + 0.0F, 0.0F);
 
-					arm.rotateAngleX = modelBipedMain.bipedLeftArm.rotateAngleX;
-					arm.rotateAngleY = modelBipedMain.bipedLeftArm.rotateAngleY;
-					arm.rotateAngleZ = modelBipedMain.bipedLeftArm.rotateAngleZ;
+				arm.rotateAngleX = modelBipedMain.bipedLeftArm.rotateAngleX;
+				arm.rotateAngleY = modelBipedMain.bipedLeftArm.rotateAngleY;
+				arm.rotateAngleZ = modelBipedMain.bipedLeftArm.rotateAngleZ;
 
-					if (data.doubleEngaged && renderSwingProgress(player, event.partialRenderTick) > 0.0F)
+				if (data.doubleEngaged && renderSwingProgress(player, event.partialRenderTick) > 0.0F)
+				{
+					float f6 = renderSwingProgress(player, event.partialRenderTick);
+					modelBipedMain.bipedBody.rotateAngleY = MathHelper.sin(MathHelper.sqrt_float(f6) * (float) Math.PI * 2.0F) * 0.2F;
+					modelBipedMain.bipedRightArm.rotationPointZ = MathHelper.sin(modelBipedMain.bipedBody.rotateAngleY) * 5.0F;
+					modelBipedMain.bipedRightArm.rotationPointX = -MathHelper.cos(modelBipedMain.bipedBody.rotateAngleY) * 5.0F;
+					arm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY;
+					modelBipedMain.bipedRightArm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY;
+					modelBipedMain.bipedRightArm.rotateAngleX -= modelBipedMain.bipedBody.rotateAngleY;
+					f6 = 1.0F - renderSwingProgress(player, event.partialRenderTick);
+					f6 *= f6;
+					f6 *= f6;
+					f6 = 1.0F - f6;
+					float f7 = MathHelper.sin(f6 * (float) Math.PI);
+					float f8 = MathHelper.sin(renderSwingProgress(player, event.partialRenderTick) * (float) Math.PI) * -(modelBipedMain.bipedHead.rotateAngleX - 0.7F) * 0.75F;
+					arm.rotateAngleX = (float) ((double) arm.rotateAngleX - ((double) f7 * 1.2D + (double) f8));
+					arm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY * 2.0F;
+					arm.rotateAngleZ = -MathHelper.sin(renderSwingProgress(player, event.partialRenderTick) * (float) Math.PI) * -0.4F;
+				}
+
+				arm.render(0.0625F);
+
+				modelBipedMain.bipedLeftArm.showModel = false;
+
+				ItemStack stack = data.doubleEngaged ? data.secondItem : null;
+				// modelBipedMain.heldItemLeft = stack == null ? 0 : 1;
+
+				if (stack != null)
+				{
+					GL11.glPushMatrix();
+					arm.postRender(0.0625F);
+					GL11.glTranslatef(0.0625F, 0.4375F, 0.0625F);
+
+					if (player.fishEntity != null)
 					{
-						float f6 = renderSwingProgress(player, event.partialRenderTick);
-						modelBipedMain.bipedBody.rotateAngleY = MathHelper.sin(MathHelper.sqrt_float(f6) * (float) Math.PI * 2.0F) * 0.2F;
-						modelBipedMain.bipedRightArm.rotationPointZ = MathHelper.sin(modelBipedMain.bipedBody.rotateAngleY) * 5.0F;
-						modelBipedMain.bipedRightArm.rotationPointX = -MathHelper.cos(modelBipedMain.bipedBody.rotateAngleY) * 5.0F;
-						arm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY;
-						modelBipedMain.bipedRightArm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY;
-						modelBipedMain.bipedRightArm.rotateAngleX -= modelBipedMain.bipedBody.rotateAngleY;
-						f6 = 1.0F - renderSwingProgress(player, event.partialRenderTick);
-						f6 *= f6;
-						f6 *= f6;
-						f6 = 1.0F - f6;
-						float f7 = MathHelper.sin(f6 * (float) Math.PI);
-						float f8 = MathHelper.sin(renderSwingProgress(player, event.partialRenderTick) * (float) Math.PI) * -(modelBipedMain.bipedHead.rotateAngleX - 0.7F) * 0.75F;
-						arm.rotateAngleX = (float) ((double) arm.rotateAngleX - ((double) f7 * 1.2D + (double) f8));
-						arm.rotateAngleY -= modelBipedMain.bipedBody.rotateAngleY * 2.0F;
-						arm.rotateAngleZ = -MathHelper.sin(renderSwingProgress(player, event.partialRenderTick) * (float) Math.PI) * -0.4F;
+						stack = new ItemStack(Item.stick);
 					}
 
-					arm.render(0.0625F);
+					EnumAction enumaction = null;
 
-					modelBipedMain.bipedLeftArm.showModel = false;
-
-					ItemStack stack = data.doubleEngaged ? data.secondItem : null;
-
-					if (stack == null)
+					if (player.getItemInUseCount() > 0)
 					{
-						modelBipedMain.heldItemLeft = 0;
+						enumaction = stack.getItemUseAction();
 					}
 
-					if (stack != null)
+					float f11;
+
+					IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(stack, EQUIPPED);
+					boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, stack, BLOCK_3D));
+					boolean isBlock = stack.itemID < Block.blocksList.length && stack.getItemSpriteNumber() == 0;
+
+					if (is3D || (isBlock && RenderBlocks.renderItemIn3d(Block.blocksList[stack.itemID].getRenderType())))
 					{
-						modelBipedMain.heldItemLeft = 1;
-
-						GL11.glPushMatrix();
-						arm.postRender(0.0625F);
-						GL11.glTranslatef(0.0625F, 0.4375F, 0.0625F);
-
-						if (player.fishEntity != null)
-						{
-							stack = new ItemStack(Item.stick);
-						}
-
-						EnumAction enumaction = null;
-
-						if (player.getItemInUseCount() > 0)
-						{
-							enumaction = stack.getItemUseAction();
-						}
-
-						float f11;
-
-						IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(stack, EQUIPPED);
-						boolean is3D = (customRenderer != null && customRenderer.shouldUseRenderHelper(EQUIPPED, stack, BLOCK_3D));
-						boolean isBlock = stack.itemID < Block.blocksList.length && stack.getItemSpriteNumber() == 0;
-
-						if (is3D || (isBlock && RenderBlocks.renderItemIn3d(Block.blocksList[stack.itemID].getRenderType())))
-						{
-							f11 = 0.5F;
-							GL11.glTranslatef(0.0F, 0.1875F, -0.3125F);
-							f11 *= 0.75F;
-							GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
-							GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-							GL11.glScalef(-f11, -f11, f11);
-						}
-						else if (stack.itemID == Item.bow.itemID)
-						{
-							f11 = 0.625F;
-							GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
-							GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
-							GL11.glScalef(f11, -f11, f11);
-							GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-							GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-						}
-						else if (Item.itemsList[stack.itemID].isFull3D())
-						{
-							f11 = 0.625F;
-
-							if (Item.itemsList[stack.itemID].shouldRotateAroundWhenRendering())
-							{
-								GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-								GL11.glTranslatef(0.0F, -0.125F, 0.0F);
-							}
-
-							if (player.getItemInUseCount() > 0 && enumaction == EnumAction.block)
-							{
-								GL11.glTranslatef(0.05F, 0.0F, -0.1F);
-								GL11.glRotatef(-50.0F, 0.0F, 1.0F, 0.0F);
-								GL11.glRotatef(-10.0F, 1.0F, 0.0F, 0.0F);
-								GL11.glRotatef(-60.0F, 0.0F, 0.0F, 1.0F);
-							}
-
-							GL11.glTranslatef(0.0F, 0.1875F, 0.0F);
-							GL11.glScalef(f11, -f11, f11);
-							GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
-							GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
-						}
-						else
-						{
-							f11 = 0.375F;
-							GL11.glTranslatef(0.1F, 0.1875F, -0.1875F);
-							GL11.glScalef(f11, f11, f11);
-							GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
-							GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
-							GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);
-						}
-
-						float f12;
-						float f13;
-						int j;
-
-						if (stack.getItem().requiresMultipleRenderPasses())
-						{
-							for (j = 0; j < stack.getItem().getRenderPasses(stack.getItemDamage()); ++j)
-							{
-								int k = stack.getItem().getColorFromItemStack(stack, j);
-								f13 = (float) (k >> 16 & 255) / 255.0F;
-								f12 = (float) (k >> 8 & 255) / 255.0F;
-								float f6 = (float) (k & 255) / 255.0F;
-								GL11.glColor4f(f13, f12, f6, 1.0F);
-								renderManager.itemRenderer.renderItem(player, stack, j);
-							}
-						}
-						else
-						{
-							j = stack.getItem().getColorFromItemStack(stack, 0);
-							float f14 = (float) (j >> 16 & 255) / 255.0F;
-							f13 = (float) (j >> 8 & 255) / 255.0F;
-							f12 = (float) (j & 255) / 255.0F;
-							GL11.glColor4f(f14, f13, f12, 1.0F);
-							renderManager.itemRenderer.renderItem(player, stack, 0);
-						}
-
-						GL11.glPopMatrix();
+						f11 = 0.5F;
+						GL11.glTranslatef(0.0F, 0.1875F, -0.3125F);
+						f11 *= 0.75F;
+						GL11.glRotatef(20.0F, 1.0F, 0.0F, 0.0F);
+						GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+						GL11.glScalef(-f11, -f11, f11);
 					}
+					else if (stack.itemID == Item.bow.itemID)
+					{
+						f11 = 0.625F;
+						GL11.glTranslatef(0.0F, 0.125F, 0.3125F);
+						GL11.glRotatef(-20.0F, 0.0F, 1.0F, 0.0F);
+						GL11.glScalef(f11, -f11, f11);
+						GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
+						GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+					}
+					else if (Item.itemsList[stack.itemID].isFull3D())
+					{
+						f11 = 0.625F;
+
+						if (Item.itemsList[stack.itemID].shouldRotateAroundWhenRendering())
+						{
+							GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+							GL11.glTranslatef(0.0F, -0.125F, 0.0F);
+						}
+
+						if (player.getItemInUseCount() > 0 && enumaction == EnumAction.block)
+						{
+							GL11.glTranslatef(0.05F, 0.0F, -0.1F);
+							GL11.glRotatef(-50.0F, 0.0F, 1.0F, 0.0F);
+							GL11.glRotatef(-10.0F, 1.0F, 0.0F, 0.0F);
+							GL11.glRotatef(-60.0F, 0.0F, 0.0F, 1.0F);
+						}
+
+						GL11.glTranslatef(0.0F, 0.1875F, 0.0F);
+						GL11.glScalef(f11, -f11, f11);
+						GL11.glRotatef(-100.0F, 1.0F, 0.0F, 0.0F);
+						GL11.glRotatef(45.0F, 0.0F, 1.0F, 0.0F);
+					}
+					else
+					{
+						f11 = 0.375F;
+						GL11.glTranslatef(0.1F, 0.1875F, -0.1875F);
+						GL11.glScalef(f11, f11, f11);
+						GL11.glRotatef(60.0F, 0.0F, 0.0F, 1.0F);
+						GL11.glRotatef(-90.0F, 1.0F, 0.0F, 0.0F);
+						GL11.glRotatef(20.0F, 0.0F, 0.0F, 1.0F);
+					}
+
+					float f12;
+					float f13;
+					int j;
+
+					if (stack.getItem().requiresMultipleRenderPasses())
+					{
+						for (j = 0; j < stack.getItem().getRenderPasses(stack.getItemDamage()); ++j)
+						{
+							int k = stack.getItem().getColorFromItemStack(stack, j);
+							f13 = (float) (k >> 16 & 255) / 255.0F;
+							f12 = (float) (k >> 8 & 255) / 255.0F;
+							float f6 = (float) (k & 255) / 255.0F;
+							GL11.glColor4f(f13, f12, f6, 1.0F);
+							renderManager.itemRenderer.renderItem(player, stack, j);
+						}
+					}
+					else
+					{
+						j = stack.getItem().getColorFromItemStack(stack, 0);
+						float f14 = (float) (j >> 16 & 255) / 255.0F;
+						f13 = (float) (j >> 8 & 255) / 255.0F;
+						f12 = (float) (j & 255) / 255.0F;
+						GL11.glColor4f(f14, f13, f12, 1.0F);
+						renderManager.itemRenderer.renderItem(player, stack, 0);
+					}
+
+					GL11.glPopMatrix();
+				}
 			}
 			else
 			{
