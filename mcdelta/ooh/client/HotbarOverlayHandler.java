d0aa1543bb665e9a9cf14a3552e8073d1f839736
TuxCraft <tux.craft.42@gmail.com>
2013-12-14 22:26:04 -0500

    12/14 10:26

--- mcdelta/ooh/client/HotbarOverlayHandler.java
+++ mcdelta/ooh/client/HotbarOverlayHandler.java
@@ -1,11 +1,16 @@
 package mcdelta.ooh.client;
 
+import static mcdelta.ooh.OOH.*;
+
 import mcdelta.ooh.OOHData;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
@@ -16,9 +21,10 @@ import org.lwjgl.opengl.GL12;
 
 public class HotbarOverlayHandler
 {
-	private static final ResourceLocation	OVERLAY	= new ResourceLocation("ooh", "textures/gui/overlay.png");
-	private static final ResourceLocation	WIDGITS	= new ResourceLocation("textures/gui/widgets.png");
-	private int	                          zLevel	= 100;
+	private static final ResourceLocation	OVERLAY	   = new ResourceLocation("ooh", "textures/gui/overlay.png");
+	private static final ResourceLocation	WIDGITS	   = new ResourceLocation("textures/gui/widgets.png");
+	protected static final RenderItem	  itemRenderer	= new RenderItem();
+	private int	                          zLevel	   = 100;
 
 
 
@@ -28,39 +34,42 @@ public class HotbarOverlayHandler
 	{
 		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
 		OOHData data = OOHData.getOOHData(player);
-		
+
 		if (data != null && event.type == ElementType.HOTBAR && data.doubleEngaged)
 		{
-			event.setCanceled(true);
+			//event.setCanceled(true);
+
+			int i = player.inventory.currentItem;
+			player.inventory.currentItem = -200;
 			
-			float height = event.resolution.getScaledHeight();
-			float width = event.resolution.getScaledWidth();
-					
+			/**int height = event.resolution.getScaledHeight();
+			int width = event.resolution.getScaledWidth();
+
 			Minecraft.getMinecraft().mcProfiler.startSection("actionBar");
 
-	        GL11.glEnable(GL11.GL_BLEND);
-	        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
-	        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-	        Minecraft.getMinecraft().renderEngine.bindTexture(WIDGITS);
+			GL11.glEnable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+			Minecraft.getMinecraft().renderEngine.bindTexture(WIDGITS);
 
-	        InventoryPlayer inv = Minecraft.getMinecraft().thePlayer.inventory;
-	        drawTexturedModalRect(width / 2 - 91, height - 22, 0, 0, 182, 22);
-	        drawTexturedModalRect(width / 2 - 91 - 1 + inv.currentItem * 20, height - 22 - 1, 0, 22, 24, 22);
+			InventoryPlayer inv = Minecraft.getMinecraft().thePlayer.inventory;
+			drawTexturedModalRect(width / 2 - 91, height - 22, 0, 0, 182, 22);
+			drawTexturedModalRect(width / 2 - 91 - 1 + inv.currentItem * 20, height - 22 - 1, 0, 22, 24, 22);
 
-	        GL11.glDisable(GL11.GL_BLEND);
-	        GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-	        RenderHelper.enableGUIStandardItemLighting();
+			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glEnable(GL12.GL_RESCALE_NORMAL);
+			RenderHelper.enableGUIStandardItemLighting();
 
-	        for (int i = 0; i < 9; ++i)
-	        {
-	            int x = width / 2 - 90 + i * 20 + 2;
-	            int z = height - 16 - 3;
-	            renderInventorySlot(i, x, z, event.partialTicks);
-	        }
+			for (int i = 0; i < 9; ++i)
+			{
+				int x = width / 2 - 90 + i * 20 + 2;
+				int z = height - 16 - 3;
+				renderInventorySlot(i, x, z, event.partialTicks);
+			}
 
-	        RenderHelper.disableStandardItemLighting();
-	        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-	        Minecraft.getMinecraft().mcProfiler.endSection();
+			RenderHelper.disableStandardItemLighting();
+			GL11.glDisable(GL12.GL_RESCALE_NORMAL);
+			Minecraft.getMinecraft().mcProfiler.endSection();*/
 		}
 	}
 
@@ -78,5 +87,37 @@ public class HotbarOverlayHandler
 		tessellator.addVertexWithUV((double) (par1 + par5), (double) (par2 + 0), (double) this.zLevel, (double) ((float) (par3 + par5) * f), (double) ((float) (par4 + 0) * f1));
 		tessellator.addVertexWithUV((double) (par1 + 0), (double) (par2 + 0), (double) this.zLevel, (double) ((float) (par3 + 0) * f), (double) ((float) (par4 + 0) * f1));
 		tessellator.draw();
+	}
+
+
+
+
+	protected void renderInventorySlot (int par1, int par2, int par3, float par4)
+	{
+		ItemStack itemstack = Minecraft.getMinecraft().thePlayer.inventory.mainInventory[par1];
+
+		if (itemstack != null)
+		{
+			float f1 = (float) itemstack.animationsToGo - par4;
+
+			if (f1 > 0.0F)
+			{
+				GL11.glPushMatrix();
+				float f2 = 1.0F + f1 / 5.0F;
+				GL11.glTranslatef((float) (par2 + 8), (float) (par3 + 12), 0.0F);
+				GL11.glScalef(1.0F / f2, (f2 + 1.0F) / 2.0F, 1.0F);
+				GL11.glTranslatef((float) (-(par2 + 8)), (float) (-(par3 + 12)), 0.0F);
+			}
+
+			log(itemstack);
+			itemRenderer.renderItemAndEffectIntoGUI(Minecraft.getMinecraft().fontRenderer, Minecraft.getMinecraft().getTextureManager(), itemstack, par2, par3);
+
+			if (f1 > 0.0F)
+			{
+				GL11.glPopMatrix();
+			}
+
+			itemRenderer.renderItemOverlayIntoGUI(Minecraft.getMinecraft().fontRenderer, Minecraft.getMinecraft().getTextureManager(), itemstack, par2, par3);
+		}
 	}
 }
